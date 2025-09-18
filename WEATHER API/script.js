bbf964a404b107ef9818952cc489d1d7798a8460
MEMBA, Aku <104360361+Rholla999@users.noreply.github.com>
2025-09-18 21:58:48 +0100

    ADDED WEATHER LOGO

--- WEATHER API/script.js
+++ WEATHER API/script.js
@@ -6,12 +6,20 @@ const descriptionEl = document.getElementById("description");
 const humidityEl = document.getElementById("humidity");
 const windSpeedEl = document.getElementById("windSpeed");
 const cityNameEl = document.getElementById("cityName");
+const weatherIconEl = document.getElementById("weatherIcon");
+const timeEl = document.getElementById('timeDisplay');
+const dayEl = document.getElementById('dayEl');
 
 
 let apiKey = '9505fd1df737e20152fbd78cdb289b6a'; 
 let url = "https://api.openweathermap.org/data/2.5/weather?units=metric&appid=" + apiKey;
 
 
+let now = new Date();
+
+timeEl.innerHTML = now.toLocaleDateString();
+dayEl.innerHTML = now.toLocaleDateString('en-us', {weekday: 'long'});
+ 
 formEl.addEventListener("submit", (e) => { 
     e.preventDefault();
     if (userCityInput.value != "") {
@@ -31,12 +39,14 @@ async function getWeather() {
             descriptionEl.textContent = weatherDataRes.weather[0].description;
             humidityEl.textContent =  weatherDataRes.main.humidity + "%";
             windSpeedEl.textContent = weatherDataRes.wind.speed + " m/s";
+            weatherIconEl.src = "http://openweathermap.org/img/wn/" + weatherDataRes.weather[0].icon + ".png";
         } else {
             cityNameEl.textContent = "City Not Found";
             temperatureEl.textContent = "-";
             descriptionEl.textContent = "-";
             humidityEl.textContent = "-";
             windSpeedEl.textContent = "-";
+            weatherIconEl.src = "";
         }
 
 
@@ -47,6 +57,7 @@ async function getWeather() {
         descriptionEl.textContent = "-";
         humidityEl.textContent = "-";
         windSpeedEl.textContent = "-";
+        weatherIconEl.src = "";
     }
 }
 
